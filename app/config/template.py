aa94422d0a5723d58ab45409e56c776e6cfe638f
Maria Bohorquez <gabriellabohorquez@gmail.com>
2023-09-28 12:29:20 -0300

    Fix prompt sometimes enumerating list

--- app/config/template.py
+++ app/config/template.py
@@ -1,8 +1,8 @@
 DIALOGUE_GENERATOR = """
-With the following game context: 
+With the following game context:
 {game_context}.
 Taking into account that you are the following character:
 Name is {character_name}, description is {character_description}.
 Give me some videogame bites that character would say in {additional_context}, taking into account this character is {character_traits}.
-Give me {number_of_lines} lines max. Be creative. Lines should be separated by a new line.
+Give me {number_of_lines} lines max. Be creative. Lines should be separated by a new line. Do not enumerate them.
 """
